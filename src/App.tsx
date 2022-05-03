025d91f13ea1733ceb798f8edaf9b7cee12b98a0
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2022-05-03 01:58:01 -0300

    added theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import Home from "./features/home/Home";
+import { ThemeProvider } from "@mui/material";
+import React, { ReactElement } from "react";
+import Home from "./app/presentation/pages/Home";
+import { theme } from "./app/style/theme";
 
-function App(): JSX.Element {
+function App(): ReactElement {
   return (
     <div className="App">
-      <Home />
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/app/style/theme.ts
@@ -0,0 +1,39 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2f3640",
+      light: "#58606b",
+      dark: "#07101a",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#66c9d9",
+      light: "#9bfcff",
+      dark: "#2b98a8",
+      contrastText: "#000",
+    },
+    error: {
+      main: "#f44336",
+      contrastText: "#fff",
+    },
+    warning: {
+      main: "#ff9800",
+      contrastText: "#fff",
+    },
+    info: {
+      main: "#2196f3",
+      contrastText: "#fff",
+    },
+    success: {
+      main: "#4caf50",
+      contrastText: "#fff",
+    },
+    text: {
+      primary: "#fff",
+      secondary: "#000",
+      disabled: "#bdbdbd",
+    },
+  },
+});
