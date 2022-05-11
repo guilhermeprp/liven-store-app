01e6df62de3cacee1ad612e4ae1b0fc9b30bfe36
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2022-05-11 03:05:16 -0300

    Added custom store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import App from "./App";
 import "./index.css";
-import { store } from "./app/store";
 import { Provider } from "react-redux";
 
 import { createRoot } from "react-dom/client";
+import { store } from "./data/store";
 
 const container = document.getElementById("root");
 
