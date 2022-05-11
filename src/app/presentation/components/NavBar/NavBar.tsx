03a426fda78ff131749fe9022a4df03dc0a939f2
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2022-05-11 03:02:47 -0300

    navbar component

--- src/app/presentation/components/NavBar/NavBar.tsx
+++ src/app/presentation/components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement } from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { ReactElement, useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -6,17 +7,31 @@ import {
   Typography,
   Box,
   Input,
-  Button,
+  Badge,
   Stack,
 } from "@mui/material";
 import * as Icon from "@mui/icons-material";
 import { Dropdown } from "../Dropdown/Dropdown";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../../data/store";
+import { refreshCount } from "../../../../controllers/cart/cart.action";
+import { getLocalStorage } from "../../../../infra/localStorage/getLocalStorage";
 
 export default function NavBar(): ReactElement {
+  const counter = useSelector(
+    (state: RootState) => state.countCartItemsReducer
+  );
+
+  const [first, setfirst] = useState([]);
+
+  const products = () => setfirst(getLocalStorage("cart"));
+
+  const dispatch = useDispatch();
+
   return (
-    <AppBar id="mainNav" color="primary">
+    <AppBar id="mainNav" sx={{ backgroundColor: "primary.dark" }}>
       <Toolbar sx={{ gap: "10px" }}>
-        <IconButton>
+        <IconButton sx={{ color: "primary.contrastText" }}>
           <Icon.Menu />
         </IconButton>
         <Typography variant="h5">Logo</Typography>
@@ -31,12 +46,35 @@ export default function NavBar(): ReactElement {
             inputProps={{ "aria-label": "pesquisar" }}
           />
         </Box>
-        <IconButton>
+        <IconButton sx={{ color: "primary.contrastText" }}>
           <Icon.Person />
         </IconButton>
-        <IconButton>
-          <Icon.ShoppingCart />
-        </IconButton>
+        <Box>
+          <IconButton
+            sx={{ color: "primary.contrastText" }}
+            onClick={() => products()}
+          >
+            <Badge badgeContent={counter.value} color="error">
+              <Icon.ShoppingCart />
+            </Badge>
+          </IconButton>
+
+          <Box
+            sx={{
+              position: "absolute",
+              top: "100%",
+              right: 0,
+              backgroundColor: "primary.dark",
+            }}
+          >
+            {first.map((product: any) => (
+              <Stack key={product.id}>
+                <Typography>{product.price}</Typography>
+                <Typography>{product.title}</Typography>
+              </Stack>
+            ))}
+          </Box>
+        </Box>
       </Toolbar>
     </AppBar>
   );
