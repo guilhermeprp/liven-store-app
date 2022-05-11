dbdba4589891a69c0b72efbbc632a49947a80144
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2022-05-11 03:03:00 -0300

    loading page

---
+++ src/app/presentation/pages/Loading.tsx
@@ -0,0 +1,21 @@
+import { Box, CircularProgress } from "@mui/material";
+import React from "react";
+
+function Loading() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "primary.main",
+      }}
+    >
+      <CircularProgress color="secondary" size="20vw" />
+    </Box>
+  );
+}
+
+export default Loading;
