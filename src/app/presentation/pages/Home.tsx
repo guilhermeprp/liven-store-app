a4624953f184aec540dfb15d0716c5e38d27516e
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2022-05-11 03:05:41 -0300

    screen loading

--- src/app/presentation/pages/Home.tsx
+++ src/app/presentation/pages/Home.tsx
@@ -1,13 +1,52 @@
 import NavBar from "../components/NavBar/NavBar";
-import React, { ReactElement } from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import React, { ReactElement, useCallback, useEffect, useState } from "react";
+import { Box, Stack } from "@mui/material";
+
+import Loading from "./Loading";
+import Error404 from "./Error404";
+import Card from "../components/Card/Card";
+import { RootState } from "../../../data/store";
+import { useSelector } from "react-redux";
+import { loadProducts } from "../../../infra/loadBalance";
 
 export default function Home(): ReactElement {
+  const loadingState = useSelector((state: RootState) => state.loadingState);
+  const productsData = useSelector(
+    (state: RootState) => state.manageProductsData
+  );
+
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  if (error) {
+    return <Error404 />;
+  }
+
+  while (loadingState.value) {
+    return <Loading />;
+  }
+
   return (
-    <Stack direction="column">
+    <Stack
+      direction="column"
+      sx={{
+        backgroundColor: "primary.dark",
+      }}
+    >
       <NavBar />
       <Box sx={{ mt: "4rem" }}>
-        <Typography variant="h4">Home</Typography>
+        <Stack
+          direction="row"
+          gap={2}
+          sx={{ flexWrap: "wrap", maxWidth: 1190, m: "0 auto", pt: "3rem" }}
+        >
+          {productsData.value.map((product: any) => (
+            <Card key={product.id} product={product} />
+          ))}
+        </Stack>
       </Box>
     </Stack>
   );
